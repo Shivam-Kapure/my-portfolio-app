54844282553bd5e5c787e14c71018ef5fa03f2f5
kkbot122 <kisnakk1503@gmail.com>
2024-11-15 13:26:43 +0530

    Initial commit

--- frontend/portfolio-app/src/pages/Auth.js
+++ frontend/portfolio-app/src/pages/Auth.js
@@ -1,22 +1,12 @@
 import React, { useState } from 'react';
 import Login from '../components/Login';
-import Register from '../components/Register';
 import '../components/Auth.css'; // Include CSS styling for Login and Register components
 
 function Auth() {
-  const [isLogin, setIsLogin] = useState(true);
-
-  const toggleAuthMode = () => {
-    setIsLogin(!isLogin);
-  };
 
   return (
-    <div className={isLogin ? 'auth-container login-bg' : 'auth-container register-bg'}>
-      {isLogin ? (
-        <Login toggleAuthMode={toggleAuthMode} />
-      ) : (
-        <Register toggleAuthMode={toggleAuthMode} />
-      )}
+    <div className= 'auth-container login-bg' >
+      <Login />
     </div>
   );
 }
